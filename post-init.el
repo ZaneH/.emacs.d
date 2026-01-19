b01f13c8b747584bd7775bb01dba433018409fac
Zane Helton <zane99@me.com>
2026-01-19 18:46:49 -0500

    Remove centaur-tabs
    Tough choice, but it takes the longest during startup. I'm also not
    convinced a tabbar is necessary in Emacs.

--- post-init.el
+++ post-init.el
@@ -840,62 +840,6 @@
   :init
   (marginalia-mode))
 
-(use-package centaur-tabs
-  :straight t
-  :config
-  (setq centaur-tabs-style "bar"
-        centaur-tabs-set-bar 'left
-        x-underline-at-descent-line t
-        centaur-tabs-gray-out-icons 'buffer
-        centaur-tabs-icon-type 'all-the-icons
-        centaur-tabs-set-icons t
-        centaur-tabs-height 32
-        centaur-tabs-set-modified-marker t
-        centaur-tabs-show-count nil)
-  (centaur-tabs-change-fonts (face-attribute 'default :font) 110)
-  (centaur-tabs-mode t)
-  (centaur-tabs-headline-match)
-  (setq uniquify-separator "/")
-  (setq uniquify-buffer-name-style 'forward)
-  
-  (defun centaur-tabs-buffer-groups ()
-    "`centaur-tabs-buffer-groups' control buffers' group rules.
-
-      Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-      All buffer name start with * will group to \"Emacs\".
-      Other buffer group by `centaur-tabs-get-group-name' with project name."
-    (list
-     (cond
-      ((or (string-equal "*" (substring (buffer-name) 0 1))
-           (memq major-mode '(magit-process-mode
-                              magit-status-mode
-                              magit-diff-mode
-                              magit-log-mode
-                              magit-file-mode
-                              magit-blob-mode
-                              magit-blame-mode)))
-       "Emacs")
-      ((derived-mode-p 'prog-mode)
-       "Editing")
-      ((derived-mode-p 'dired-mode)
-       "Dired")
-      ((memq major-mode '(helpful-mode
-                          help-mode))
-       "Help")
-      ((memq major-mode '(org-mode
-                          org-agenda-clockreport-mode
-                          org-src-mode
-                          org-agenda-mode
-                          org-beamer-mode
-                          org-indent-mode
-                          org-bullets-mode
-                          org-cdlatex-mode
-                          org-agenda-log-mode
-                          diary-mode))
-       "OrgMode")
-      (t
-       (centaur-tabs-get-group-name (current-buffer)))))))
-
 (use-package dtrt-indent
   :straight t
   :defer t
@@ -1656,8 +1600,7 @@
     "w+"  '(evil-window-increase-height :which-key "Increase height")
     "w-"  '(evil-window-decrease-height :which-key "Decrease height")
     "w>"  '(evil-window-increase-width :which-key "Increase width")
-    "w<"  '(evil-window-decrease-width :which-key "Decrease width")
-    "wt"  '(centaur-tabs-ace-jump :which-key "Jump tabs"))
+    "w<"  '(evil-window-decrease-width :which-key "Decrease width"))
 
   ;;; evil-mc keybindings (multiple cursors)
   (my/leader
@@ -1931,10 +1874,6 @@
      :keymaps 'org-agenda-mode-map
      :states 'motion
      "C-SPC" 'org-agenda-show-and-scroll-up))
-
-  (with-eval-after-load 'centaur-tabs
-    (define-key evil-normal-state-map (kbd "g t") 'centaur-tabs-forward)
-    (define-key evil-normal-state-map (kbd "g T") 'centaur-tabs-backward))
 
   (with-eval-after-load 'dirvish
     (general-def dirvish-mode-map
