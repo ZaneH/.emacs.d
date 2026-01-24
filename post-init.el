8fba210ef6570c907558e4a2dbce40c64cb3da2d
Zane Helton <zane99@me.com>
2026-01-24 03:44:02 -0500

    Remove Circe

--- post-init.el
+++ post-init.el
@@ -1169,24 +1169,6 @@
    '(zoom-size '(0.618 . 0.618))
    '(zoom-ignored-major-modes '(vterm-mode))))
 
-(use-package circe
-  :straight t
-  :defer t
-  :init
-  (enable-circe-color-nicks)
-  (enable-circe-display-images)
-  (enable-lui-track-bar)
-  (enable-lui-irc-colors)
-  :config
-  (setq lui-flyspell-p t
-        lui-flyspell-alist '((".*" "american")))
-  (setq helm-mode-no-completion-in-region-in-modes
-        '(circe-channel-mode
-          circe-query-mode
-          circe-server-mode))
-  (setq circe-use-cycle-completion t)
-  (setq circe-reduce-lurker-spam t))
-
 (use-package swiper-helm
   :straight t
   :defer t)
